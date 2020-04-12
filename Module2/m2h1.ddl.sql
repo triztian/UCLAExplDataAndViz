851015fe6026240f2dc8fe0ff8a42c46f32c063b
Tristian Azuara <tristian.azuara@gmail.com>
2020-04-12 00:34:35 -0700

    Add excercise SQL

---
+++ Module2/m2h1.ddl.sql
@@ -0,0 +1,23 @@
+/**
+ UCLA Data Exploration and Visualization
+ Instructor: Ali El-Anan
+
+ Module 2 - Homework 1
+ Zoom Stock Prices - Data Manipulation Language (DML)
+ Tristian Azuara (tristian.azuara@gmail.com)
+ */
+
+/* 
+CSV Data:
+Date,Open,High,Low,Close,Adj Close,Volume
+2020-01-10,73.080002,73.800003,72.250000,73.089996,73.089996,1655100
+ */
+CREATE TABLE zm_data (
+	date 			TEXT PRIMARY KEY,
+	open 			REAL NOT NULL,
+	high 			REAL NOT NULL,
+	low				REAL NOT NULL,
+	close			REAL NOT NULL,
+	adjusted_close	REAL NOT NULL,
+	volume			INTEGER NOT NULL
+);
---
+++ Module2/m2h1.dml.sql
@@ -0,0 +1,49 @@
+/**
+ UCLA Data Exploration and Visualization
+ Instructor: Ali El-Anan
+
+ Module 2 - Homework 1
+ Zoom Stock Prices - Data Manipulation Language (DML)
+ Tristian Azuara (tristian.azuara@gmail.com)
+ */
+
+/*
+ Make sure that you've compiled the extension.
+ */
+.load ./libsqlitefunctions.dylib
+
+/**
+ Import the CSV data, we must defined the separator otherwise
+ the import will fail.
+
+ Make sure that the CSV file does not contain header row because it will
+ be imported by the command; introducing invalid data.
+
+ Header rows typically look like this:
+
+	#Date,Open,High,Low,Close,Adj Close,Volume
+
+ */
+.separator ','
+.import ./zm_01-10_04-09.csv zm_data 
+
+/**
+ Verify that the data was imported. The count should match the output of the
+ `wc` command ie, if we count the number of lines in the CSV files (without the 
+ header row) the result of the following count should match, to count the lines
+ in the CSV file you can use:
+
+	$ wc zm_01-10_04-09.csv
+
+ */
+SELECT COUNT(*) FROM zm_data LIMIT 10;
+
+/**
+ Calculate the median, standard deviation and average of the open price, the 
+ adjusted closing price and the trading volume respectively.
+ */
+SELECT 
+	MEDIAN(open), 
+	STDEV(adjusted_close), 
+	AVG(volume) 
+FROM zm_data;
