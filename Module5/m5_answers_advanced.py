a852b9b46304f8fcf3f21580fd477780f030d5d6
Tristian Azuara <tristian.azuara@gmail.com>
2020-04-19 00:09:26 -0700

    Module5 and Module2 bundle upload
    Former-commit-id: 051909616bcec4a65aa8e106b8623aeb84eb26e4

---
+++ Module5/m5_answers_advanced.py
@@ -0,0 +1,30 @@
+#**********************
+# JSON, CSV files in Python
+#**********************
+
+#E1
+# import JSON, inspect the data
+# load json file from Module 4 (copied over for convenience)
+import json
+from os import path
+
+#E2
+# import CSV, inspect the data
+# load csv file from Module 1 (copied over for convenience)
+
+#**********************
+# Pandas and native graphing in Python
+#**********************
+
+#E3
+# Import JSON to pandas
+
+#E4
+#Import HTML to pandas
+
+#E5
+# Analyze some of the data
+
+#E6
+# Graph some of the data
+
